8522f5adb207e171a5d0be7270ea3be9e1f20dae
michavol <michavol@student.ethz.ch>
2022-06-26 23:51:16 +0200

    generate bulk results

--- BashScripts/run.sh
+++ BashScripts/run.sh
@@ -4,16 +4,16 @@ cd ..
 #echo "Running config_tcga"
 #python BLRunner.py --config config-files/config_tcga.yaml
 
-echo "Running config_dream4_10\n"
-python BLRunner.py --config config-files/config_dream4_10.yaml
+# echo "Running config_dream4_10\n"
+# python BLRunner.py --config config-files/config_dream4_10.yaml
 
 # echo "Running config_dream4_100\n"
 # python BLRunner.py --config config-files/config_dream4_100.yaml
+# python BLEvaluator.py --config config-files/config_dream4_10.yaml -n -z -y -t -e -x -j -a -u 
+# python BLEvaluator.py --config config-files/config_dream4_100.yaml -n -z -y -t -e -x -j -a -u 
 
 # echo "Running config_sergio_bulk_100\n"
 # python BLRunner.py --config config-files/config_sergio_bulk_100.yaml
-
-# python BLEvaluator.py --config config-files/config_dream4_100.yaml -n -z -y -t -e -x -j #-a -u 
 
 # echo "Running config_sergio_100\n"
 # python BLRunner.py --config config-files/config_sergio_100.yaml
@@ -21,8 +21,8 @@ python BLRunner.py --config config-files/config_dream4_10.yaml
 # echo "Running config_sergio_400\n"
 # python BLRunner.py --config config-files/config_sergio_400.yaml
 
-# echo "Running config_sergio_bulk_400\n"
-# python BLRunner.py --config config-files/config_sergio_bulk_400.yaml
+echo "Running config_sergio_bulk_400\n"
+python BLRunner.py --config config-files/config_sergio_bulk_400.yaml
 
 #echo "Running config_sergio_1200\n"
 # python BLRunner.py --config config-files/config_sergio_1200.yaml
